acc6314601294c3085fa2edfd8b9bdf841a4839c
Manhar <manhardangar0@gmail.com>
2021-11-24 11:26:45 +0530

    add to cart functionality

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
-const express = require('express');
+require('dotenv').config()
+const express = require('express')
 const app = express();
 const esj = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose');
+const session = require("express-session")
+const flash = require("express-flash")
+const MongoDbStore = require("connect-mongo")
+
+
+// Database connection
+ mongoose.connect('mongodb://localhost/pizza')
+.then((result) => console.log('Database connected...'))
+.catch((err) => console.log(err))
+
+//session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave:false,
+    store: MongoDbStore.create({
+        mongoUrl:process.env.MONGO_CONNECTION_URL 
+    }),
+    cookie: { maxAge:100 * 60 * 60 * 24 }
+}))
+
+app.use(flash())
 
 //Assets
 app.use(express.static('public'))
-
+app.use(express.json())
 //set template engine
 app.use(expressLayout)
 
-
+//Global Middleware
+app.use((req,res,next) => {
+    res.locals.session = req.session
+    next()
+})
 
 app.set('views', path.join(__dirname,'/resources/views'))
 app.set('view engine', 'ejs')
